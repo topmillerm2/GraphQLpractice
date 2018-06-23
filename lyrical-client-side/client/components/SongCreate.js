bc27b8530890e445641fb2a1f27688a7509220b4
Michael <michaeltopmiller@gmail.com>
2018-06-23 15:05:51 -0700

    update master

--- lyrical-client-side/client/components/SongCreate.js
+++ lyrical-client-side/client/components/SongCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import gql from 'graphql-tag';
 
 class SongCreate extends Component {
   constructor() {
@@ -8,11 +9,15 @@ class SongCreate extends Component {
     };
   }
 
+  onSubmit = event => {
+    event.preventDefault();
+  };
+
   render() {
     return (
       <div>
         <h3>Create a New Song</h3>
-        <form>
+        <form onSubmit={this.onSubmit}>
           <label>Song Title</label>
           <input
             onChange={event => this.setState({ title: event.target.value })}
@@ -23,5 +28,11 @@ class SongCreate extends Component {
     );
   }
 }
+
+const mutation = gql`
+	mutation {
+		addSong(title: )
+	}
+`;
 
 export default SongCreate;
